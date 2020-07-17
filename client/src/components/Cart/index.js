55e92a3dd4f95077273fb2222d22df3d3dcf3424
tom2u <tbrodhead@2u.com>
2020-07-17 14:38:04 -0500

    22.4.6

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,12 +4,17 @@ import Auth from "../../utils/auth";
 import "./style.css";
 import { useStoreContext } from "../../utils/GlobalState";
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from "../../utils/actions";
-
 import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from "../../utils/queries";
+import { loadStripe } from "@stripe/stripe-js";
+import { useLazyQuery } from "@apollo/react-hooks";
+
+const stripePromise = loadStripe("pk_test_TYooMQauvdEDq54NiTphI7jx");
 
 const Cart = () => {
   const [state, dispatch] = useStoreContext();
-
+  const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+  /*
   useEffect(() => {
     async function getCart() {
       const cart = await idbPromise("cart", "get");
@@ -20,6 +25,14 @@ const Cart = () => {
       getCart();
     }
   }, [state.cart.length, dispatch]);
+*/
+  useEffect(() => {
+    if (data) {
+      stripePromise.then((res) => {
+        res.redirectToCheckout({ sessionId: data.checkout.session });
+      });
+    }
+  }, [data]);
 
   console.log(state);
 
@@ -33,6 +46,18 @@ const Cart = () => {
       sum += item.price * item.purchaseQuantity;
     });
     return sum.toFixed(2);
+  }
+
+  function submitCheckout() {
+    const productIds = [];
+    getCheckout({
+      variables: { products: productIds },
+    });
+    state.cart.forEach((item) => {
+      for (let i = 0; i < item.purchaseQuantity; i++) {
+        productIds.push(item._id);
+      }
+    });
   }
 
   if (!state.cartOpen) {
@@ -59,7 +84,7 @@ const Cart = () => {
           <div className="flex-row space-between">
             <strong>Total: ${calculateTotal()}</strong>
             {Auth.loggedIn() ? (
-              <button>Checkout</button>
+              <button onClick={submitCheckout}>Checkout</button>
             ) : (
               <span>(log in to check out)</span>
             )}
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,7 +2,6 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User, Product, Category, Order } = require("../models");
 const { signToken } = require("../utils/auth");
 const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
-const url = new URL(context.headers.referer).origin;
 
 const resolvers = {
   Query: {
@@ -57,6 +56,7 @@ const resolvers = {
       const order = new Order({ products: args.products });
       const { products } = await order.populate("products").execPopulate();
       const line_items = [];
+      const url = new URL(context.headers.referer).origin;
 
       for (let i = 0; i < products.length; i++) {
         // generate product id
